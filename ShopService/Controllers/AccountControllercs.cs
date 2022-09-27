b1aebdcfc2ea1775c074d7cc63f4c9b9422bfd9b
Кирилл Парахин <parahinkv@gmail.com>
2022-09-27 10:24:48 +0300

    Add Profile Controller

--- ShopService/Controllers/AccountControllercs.cs
+++ ShopService/Controllers/AccountControllercs.cs
@@ -3,12 +3,25 @@ using Auth0.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization; 
 using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using ShopService.Models;
 
 
 namespace ShopService.Controllers
 {
     public class AccountControllercs : Controller
     {
+        [Authorize]
+        public IActionResult Profile()
+        {
+            return View(new ProfileModel()
+            {
+                Id = User.FindFirst(x => x.Type == ClaimTypes.Authentication)?.Value,
+                Name = User.Identity!.Name,
+                EmailAdress = User.FindFirst(x => x.Type == ClaimTypes.Email)?.Value,
+
+            });
+        }
         public async Task Login(string returnUrl = "/")
         {
             var authentificationProperties = new LoginAuthenticationPropertiesBuilder()
--- ShopService/Models/ProfileModel.cs
+++ ShopService/Models/ProfileModel.cs
@@ -2,7 +2,7 @@
 {
     public class ProfileModel
     {
-        public int Id { get; set; }
+        public string? Id { get; set; }
         public string? Name { get; set; }
         public string? EmailAdress { get; set; }
         public int? RoleId { get; set; }
