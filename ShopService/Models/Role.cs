46944798b3841e6e65fd1236d14e385a35d5c016
Кирилл Парахин <parahinkv@gmail.com>
2022-10-13 11:23:41 +0300

    RoleType Enum

--- ShopService/Models/Role.cs
+++ ShopService/Models/Role.cs
@@ -1,9 +1,18 @@
 ﻿namespace ShopService.Models
 {
+    public enum RoleType: byte
+    {
+        NewUser = 1,
+        AuthUser = 2,
+        Operator = 3,
+        Manager = 4,
+        Admin = 5
+    }
     public class Role
     {
         public int Id { get; set; }
-        public string? Caption { get; set; } 
+        public RoleType RoleType { get; set; }
+        public string? RoleCaption { get; set; }
         public ICollection<User>? Users { get; set; }
         public ICollection<Option>? Actions { get; set; }
     }
