ee6547ab2955d0cae03df662988d2c64aa8660ae
Tigeroff2002 <parahinkv@gmail.com>
2022-11-17 22:05:19 +0300

    add controllers

---
+++ AuthdService/controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthdService.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult PrivateData()
+        {
+            return View();
+        }
+    }
+}
