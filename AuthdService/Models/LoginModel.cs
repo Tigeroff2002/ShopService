83f0b56d086cfac03d26f4aef80d3983b5c2af4f
Kirill Parakhin <kirill.parakhin@altenar.com>
2022-11-18 11:42:47 +0300

    ReturnUrl Fix

--- AuthdService/Models/LoginModel.cs
+++ AuthdService/Models/LoginModel.cs
@@ -13,6 +13,6 @@ namespace AuthdService.Models
         [DataType(DataType.Password)]
         public string? Password { get; set; }
         public bool RememberLogin { get; set; }
-        public string? ReturnUrl { get; set; }
+        public string? ReturnUrl { get; } = "/Home/Index";
     }
 }
