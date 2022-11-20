b3e4d7a07137d7f0dbdaf0dad863ee7379aefca9
Tigeroff2002 <parahinkv@gmail.com>
2022-11-20 18:33:25 +0300

    Add new single project Data and common context

---
+++ Data/Contexts/RetailStoreDataContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Data.Contexts
+{
+    public class RetailStoreDataContext : DbContext
+    {
+        public RetailStoreDataContext(DbContextOptions<RetailStoreDataContext> options)
+            : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<DeviceType> DeviceTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Clients { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Shipping> Shippings { get; set; }
+        public DbSet<Warehouse> Warehouses { get; set; }
+        public DbSet<Shop> Shops { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<SummUpProduct> SummUpProducts { get; set; }
+    }
+}
