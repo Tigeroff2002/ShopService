6af98ba0ca6083bf9143e8ccd2ed9e780704f0c7
Tigeroff2002 <parahinkv@gmail.com>
2022-12-02 22:35:31 +0300

    Adding all to shopservice

---
+++ Models.Tests/RegisterModel.Tests.cs
@@ -0,0 +1,69 @@
+﻿using Xunit;
+
+using FluentAssertions;
+
+using Moq;
+
+using AuthdService.Models;
+
+namespace AuthdService.AuthModels.Tests
+{
+    public class RegisterModelsTests
+    {
+        [Fact(DisplayName = $"{nameof(RegisterModel)} can be created")]
+        [Trait("Category", "Unit")]
+        public void CanBeCreated()
+        {
+            // Arrange
+            var email = "parahinkv@gmail.com";
+            var password = "password";
+            var confirmPassword = "password";
+            var contactNumber = "";
+            var isRoleChoosed = false;
+            var roleType = 1;
+
+            // Act
+            var exception = Record.Exception(() => new RegisterModel(email, "", isRoleChoosed, roleType, password, confirmPassword, contactNumber));
+
+            // Assert
+            exception.Should().BeNull();
+        }
+
+        [Fact(DisplayName = $"{nameof(RegisterModel)} can not be created without email")]
+        [Trait("Category", "Unit")]
+        public void CanNotBeCreatedBecauseEmailEmpty()
+        {
+            // Arrange
+            var password = "password";
+            var confirmPassword = "password";
+            var contactNumber = "";
+            var isRoleChoosed = false;
+            var roleType = 1;
+
+            // Act
+            var exception = Record.Exception(() => new RegisterModel("", "", isRoleChoosed, roleType, password, confirmPassword, contactNumber));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentException>(nameof(String));
+        }
+
+        [Fact(DisplayName = $"{nameof(RegisterModel)} can not be created with different password and confirmPassword")]
+        [Trait("Category", "Unit")]
+        public void CanNotBeCreatedBecausePasswordDoesNotEquals()
+        {
+            // Arrange
+            var email = "parahinkv@gmail.com";
+            var password = "password1";
+            var confirmPassword = "password2";
+            var contactNumber = "";
+            var isRoleChoosed = false;
+            var roleType = 1;
+
+            // Act
+            var exception = Record.Exception(() => new RegisterModel(email, "", isRoleChoosed, roleType, password, confirmPassword, contactNumber));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentException>();
+        }
+    }
+}
---
+++ ShopService/Controllers/AccountController.cs
@@ -0,0 +1,209 @@
+﻿using AuthdService.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Data.Contexts;
+using System.Security.Claims;
+
+namespace AuthdService.Controllers
+{
+    [ApiController]
+    [Route("api/account")]
+    public class AccountController : Controller
+    {
+        public AccountController(ILogger<AccountController> logger, RetailStoreDataContext context)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+
+            _logger.LogInformation("Account Controller was started");
+
+            //SeedSomeUserData();
+        }
+
+        [HttpPost("data/add")]
+        public void SeedSomeUserData()
+        {
+            if (_context!.Clients.Count() < 4)
+            {
+                _context!.Clients.Add(new User(4, 1)
+                {
+                    Id = 4,
+                    NickName = "TigeroffNew",
+                    Password = "tigeroff2002",
+                    ContactNumber = "+79042555027",
+                    EmailAdress = "parahinkv2002@gmail.com",
+                    TotalPurchase = 0f,
+                    Discount = 0f,
+                    Role = new Role(RoleType.AuthUser)
+                });
+            }
+
+            _logger!.LogInformation("Fourth user was added to DB");
+        }
+
+        [HttpGet("login")]
+        public async Task<ActionResult> Login()
+        {
+            AuthdService.Views.Account.LoginModel objLoginModel = new();
+            //return View("Login", objLoginModel);
+            return View("Login", objLoginModel);
+        }
+
+        public async Task<ActionResult> LoginPost(
+            string nickName,
+            string password)
+        {
+            if (string.IsNullOrEmpty(nickName))
+            {
+                throw new ArgumentException(nameof(nickName));
+            }
+
+            if (string.IsNullOrEmpty(password))
+            {
+                throw new ArgumentException(nameof(password));
+            }
+
+            Views.Account.LoginModel objLoginModel = new Views.Account.LoginModel { NickName = nickName, Password = password };
+
+            if (ModelState.IsValid)
+            {
+                var user = _context!.Clients!
+                    .Where(
+                        x => x.NickName == objLoginModel.NickName &&
+                        x.Password == objLoginModel.Password)
+                    .FirstOrDefault();
+
+                if (user == null)
+                {
+                    ViewBag.Message = "Such user was not registered in system yet";
+                    return View("Register", new Views.Account.RegisterModel());
+                }
+                else
+                {
+                    var claims = new List<Claim>() {
+                    new Claim(ClaimTypes.NameIdentifier, Convert.ToString(user.Id)),
+                        new Claim(ClaimTypes.Name, user!.EmailAdress!),
+                        new Claim(ClaimTypes.Role, user!.Role!.RoleCaption!)};
+
+                    var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                    var principal = new ClaimsPrincipal(identity);
+
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties()
+                    {
+                        IsPersistent = objLoginModel.RememberLogin
+                    });
+
+                    _logger!.LogInformation("User was successfuly authorized!");
+
+                    return View("Index1");
+                }
+            }
+
+            return View("Index1");
+        }
+
+        [HttpGet("logout")]
+        public async Task<IActionResult> LogOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            _logger!.LogInformation("User was successfuly unauthorized!");
+
+            return View("Login", new Views.Account.LoginModel());
+        }
+
+        [HttpGet("register")]
+        public async Task<ActionResult> Register()
+        {
+            Views.Account.RegisterModel objRegisterModel = new();
+            return View("Register", objRegisterModel);
+        }
+
+        [HttpGet("postregister")]
+        public async Task<IActionResult> RegisterPost(
+            string email,
+            string nickName,
+            int roleType,
+            string contactNumber,
+            string password,
+            string confirmPassword)
+        {
+            Views.Account.RegisterModel objRegisterModel = new(
+                email,
+                nickName,
+                roleType,
+                contactNumber,
+                password,
+                confirmPassword);
+
+            if (ModelState.IsValid)
+            {
+                User? user = new User(_context!.Clients.Count(), 1)
+                {
+                    Id = _context!.Clients.Count(),
+                    NickName = objRegisterModel.NickName,
+                    Password = objRegisterModel?.Password,
+                    ContactNumber = objRegisterModel?.ContactNumber,
+                    EmailAdress = objRegisterModel?.Email,
+                    TotalPurchase = 0f,
+                    Discount = 0f,
+                    Role = new Role(RoleType.AuthUser)
+                };
+
+                if (user != null)
+                {
+                    if (CheckCurrentUserExistenseId(user) == -1)
+                        _context!.Clients.Add(user);
+                    else
+                    {
+                        ViewBag.Message = "Such user have already registered in system";
+                        _logger!.LogInformation("User with these data was found in system!");
+                        return View("Login", new Views.Account.LoginModel());
+                    }
+
+                    ArgumentNullException.ThrowIfNull(user!.EmailAdress);
+
+                    ArgumentNullException.ThrowIfNull(user!.Role.RoleCaption);
+
+                    var claims = new List<Claim>() {
+                    new Claim(ClaimTypes.NameIdentifier, Convert.ToString(user!.Id)),
+                        new Claim(ClaimTypes.Name, user!.EmailAdress!),
+                        new Claim(ClaimTypes.Role, user!.Role!.RoleCaption!)};
+
+                    var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                    var principal = new ClaimsPrincipal(identity);
+
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties()
+                    {
+                        IsPersistent = true
+                    });
+
+                    _logger!.LogInformation("User was successfuly registered in system!");
+
+                    return View("Index1");
+                }
+                else
+                {
+                    ViewBag.Message = "Invalid Credential";
+                    return View("Register", new Views.Account.RegisterModel());
+                }
+            }
+
+            return View("Index1");
+        }
+
+        private int CheckCurrentUserExistenseId(User? user)
+        {
+            if (_context!.Clients!.Any(x => x.Equals(user)))
+                return _context!.Clients!.FirstOrDefault(x => x.Equals(user))!.Id;
+            return -1;
+        }
+
+        private ILogger<AccountController>? _logger;
+        private RetailStoreDataContext? _context;
+    }
+}
